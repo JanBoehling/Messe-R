b55fe6c2396f4d2d4890635feb7413e8193525e9
InspectorRaupe <jonas.kluge03@gmail.com>
2023-09-10 12:23:12 +0200

    Made Blood Splatter prefab;

--- Assets/Scripts/FillingTheCorpse.cs
+++ Assets/Scripts/FillingTheCorpse.cs
@@ -9,13 +9,29 @@ public class FillingTheCorpse : MonoBehaviour
     [SerializeField] GameObject[] organsOfTheCorpse;
     public int counterOfPresses;
     public int maxPresses = 5;
+    [SerializeField] ParticleSystem particleBlood;
 
     public void SpawningThemOrgans()
     {
         organsOfTheCorpse[counterOfPresses -1].SetActive(true);
+        particleBlood.Play();
+
+
         if (counterOfPresses == maxPresses)
         {
+        StartCoroutine(WinningThemGame());
+
+        }
+    }
+
+    public IEnumerator WinningThemGame()
+    {
+        if (counterOfPresses == maxPresses)
+        {
+            Debug.Log("Corutine läuft");
+            yield return new WaitForSeconds(5f);
             GameManager.Instance.WonGame();
         }
+        yield return null;
     }
 }
